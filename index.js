40cae8fa53c66ebb4b1c3d0b120c7eb834675cad
Faridahnoor <120566807+Faridahnoor@users.noreply.github.com>
2022-12-23 13:39:48 +0100

    se

---
+++ index.js
@@ -0,0 +1,24 @@
+
+        // let menu = document.querySelector('#menu-icon');
+        // let navlist = document.querySelector('.navlist');
+        // // menu.onclick = () => {
+        // //     menu.classList.toggle('bx-x');
+        // //     navlist.classList.toggle('active');
+        // // };
+        
+        // //  window.onscroll = () => {
+        // //     menu.classList.remove('bx-x');
+        // //     navlist.classList.remove('active');
+        // //  };
+
+        // menu.onclick = () =>
+        // {
+        //     navlist.style.top="0";
+        //     menu.classList.toggle('bx-x');
+        // };
+        // window.onscroll = () => {
+        //       menu.classList.remove('bx-x');
+        //         //  navlist.classList.remove('active');
+        //       };
+    
+    
